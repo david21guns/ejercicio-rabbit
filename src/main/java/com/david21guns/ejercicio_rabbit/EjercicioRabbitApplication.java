ef48f8bffce536f95cca7c0d0be33ef623b2c37c
aldo.delgadillo <aldoddm@gmail.com>
2025-03-30 16:02:50 -0600

    base project

---
+++ src/main/java/com/david21guns/ejercicio_rabbit/EjercicioRabbitApplication.java
@@ -0,0 +1,13 @@
+package com.david21guns.ejercicio_rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioRabbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david21guns/ejercicio_rabbit/EjercicioRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david21guns.ejercicio_rabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioRabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
